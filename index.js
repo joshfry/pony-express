5333013cf6a6cba574f9e49e03912156b27f77b1
joshfry <joshdfry@me.com>
2019-12-03 12:47:12 -0800

    routes with dynamic segments

--- index.js
+++ index.js
@@ -6,12 +6,31 @@ const emails = require('./fixtures/emails');
 const app = express();
 
 const getUsersRoute = (req, res) => res.send(users);
+
+const getUserRoute = (req, res) => {
+  const user = users.find(user => user.id === req.params.id);
+  res.send(user);
+};
+
 const getEmailsRoute = (req, res) => res.send(emails);
+
+const getEmailRoute = (req, res) => {
+  const email = emails.find(email => email.id === req.params.id);
+  res.send(email);
+};
 
 const router = express.Router();
 
 router.get('/users', getUsersRoute);
+router.get('/users/:id', getUserRoute);
+
 router.get('/emails', getEmailsRoute);
+router.get('/emails/:id', getEmailRoute);
+
+
+// router.get('/emails/from/:sender/to/:recipient', (req, res) => {
+//   console.log(req.params);
+// });
 
 app.use(router);
 app.listen(3000);
