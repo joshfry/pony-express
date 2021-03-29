6699255776ae6178a9053b5b6226bcaabcd193e5
joshfry <joshdfry@me.com>
2021-03-29 16:10:26 -0700

    Create read-body util, integrate with createEmailRoute

--- routes/emails.js
+++ routes/emails.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const readBody = require('../lib/read-body');
 const emails = require('../fixtures/emails');
 
 const getEmailsRoute = (req, res) => {
@@ -10,9 +11,17 @@ const getEmailRoute = (req, res) => {
   res.send(email);
 };
 
+// since `readBody` returns a promise, this will be an async function
+const createEmailRoute = async (req, res) => {
+  const body = await readBody(req);
+  const newEmail = JSON.parse(body);
+  emails.push(newEmail);
+};
+
 const emailsRouter = express.Router();
 
 emailsRouter.get('/', getEmailsRoute);
 emailsRouter.get('/:id', getEmailRoute);
+emailsRouter.post('/', createEmailRoute);
 
 module.exports = emailsRouter;
