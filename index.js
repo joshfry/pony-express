40932a725bf9095206b99f391da519e2db088a80
joshfry <joshdfry@me.com>
2021-03-29 17:33:28 -0700

    Create and implement logger middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const logger = require('./lib/logger')
 
 const usersRouter = require('./routes/users')
 const emailsRouter = require('./routes/emails')
 
 const app = express()
 
+app.use(logger)
 app.use('/users', usersRouter)
 app.use('/emails', emailsRouter)
 
